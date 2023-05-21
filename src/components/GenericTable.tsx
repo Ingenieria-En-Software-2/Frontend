dcbed6e27359278c74f3e8e0ea19f0ad81f3a1da
chrischriscris <18-10892@usb.ve>
2023-05-21 02:26:37 -0400

    added style and row names to tables

--- src/components/GenericTable.tsx
+++ src/components/GenericTable.tsx
@@ -7,35 +7,51 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+function createData(username: string, names: string, surnames: string, role: string, usertype: string) {
+  const actions = "(Dropdown)";
+  return { username, names, surnames, role, usertype, actions };
 }
 
-const rows = [createData("Frozen yoghurt", 159, 6.0, 24, 4.0), createData("Ice cream sandwich", 237, 9.0, 37, 4.3), createData("Eclair", 262, 16.0, 24, 6.0), createData("Cupcake", 305, 3.7, 67, 4.3), createData("Gingerbread", 356, 16.0, 49, 3.9)];
+const rows = [createData("admin", "Admin", "One", "Analista de Cuentas", "Interno"), createData("user", "User", "Two", "Analista de Cuentas", "Externo"), createData("user2", "User", "Three", "Cuentahabiente", "Mixto"), createData("user3", "User", "Four", "Analista de Cuentas", "Interno"), createData("user4", "User", "Five", "Cuentahabiente", "Externo")];
 
 export default function GenericTable() {
+  const headerStyle = {
+    backgroundColor: "#3f51b5",
+    color: "#fff",
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
+      <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+        {/* Blue  */}
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell sx={headerStyle}>Usuario</TableCell>
+            <TableCell sx={headerStyle}>Nombres</TableCell>
+            <TableCell sx={headerStyle}>Apellidos</TableCell>
+            <TableCell sx={headerStyle} align="center">
+              Rol
+            </TableCell>
+            <TableCell sx={headerStyle} align="center">
+              Tipo de Usuario
+            </TableCell>
+            <TableCell sx={headerStyle} align="center">
+              Acciones
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {row.name}
+            // Alternate row color
+            <TableRow key={row.username} sx={{ "&:last-child td, &:last-child th": { border: 0 }, "&:nth-of-type(odd)": { backgroundColor: "#f5f5f5" } }}>
+              <TableCell align="center" component="th" scope="row">
+                {row.username}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell>{row.names}</TableCell>
+              <TableCell>{row.surnames}</TableCell>
+              <TableCell align="center">{row.role}</TableCell>
+              <TableCell align="center">{row.usertype}</TableCell>
+              <TableCell align="center">{row.actions}</TableCell>
             </TableRow>
           ))}
         </TableBody>
