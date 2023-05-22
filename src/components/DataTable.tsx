fce4c9a7160f28553220928bf465a65a0b22ac24
fungikami <kafung09@gmail.com>
2023-05-21 23:39:42 -0400

    Search Bar

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -7,7 +7,9 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import TablePagination from "@mui/material/TablePagination";
-import { Box } from "@mui/material";
+import { Grid, Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
 
 export interface Column {
   id: string;
@@ -37,6 +39,7 @@ interface Props {
 export default function DataTable({ title, columns, rows }: Props): JSX.Element {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(15);
+  const [visibleRows, setVisibleRows] = React.useState(rows);
 
   const handleChangePage = (_event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
     setPage(newPage);
@@ -55,10 +58,44 @@ export default function DataTable({ title, columns, rows }: Props): JSX.Element 
   const begin = page * rowsPerPage;
   const end = begin + rowsPerPage;
 
-  const visibleRows = rows.slice(begin, end);
+  React.useEffect(() => {
+    setVisibleRows(rows.slice(begin, end));
+
+    return () => {
+      setVisibleRows([]);
+    };
+  }, [rows, begin, end]);
+
+  // Search Bar
+  const requestSearch = (searchedVal: string) => {
+    const filteredRows = rows.filter((row) => {
+      return Object.keys(row).some((key) => {
+        return row[key].toString().toLowerCase().includes(searchedVal.toLowerCase());
+      });
+    });
+    setVisibleRows(filteredRows);
+  };
 
   return (
     <Box>
+      {/* Search Bar */}
+      <Grid container direction="row" justifyContent="flex-end" alignItems="center" sx={{ mb: 2 }}>
+        <Box>
+          <TextField
+            label="Search"
+            variant="outlined"
+            color="primary"
+            size="small"
+            sx={{ width: "auto", height: "auto" }}
+            placeholder="Search"
+            type="search"
+            onInput={(e) => requestSearch(e.target.value)}
+            InputProps={{
+              endAdornment: <SearchIcon sx={{ color: "action.active", my: 0.5 }} />,
+            }}
+          />
+        </Box>
+      </Grid>
       <TableContainer component={Paper} sx={{ maxHeight: 440 }}>
         <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -84,20 +121,20 @@ export default function DataTable({ title, columns, rows }: Props): JSX.Element 
               // Alternate row color
               <TableRow
                 hover
-                key={row.username}
+                key={row.id}
                 sx={{
                   "&:last-child td, &:last-child th": { border: 0 },
                   "&:nth-of-type(odd)": { backgroundColor: "#f0f0ff" },
                 }}
               >
-                <TableCell align="center" component="th" scope="row">
-                  {row.username}
-                </TableCell>
-                <TableCell>{row.names}</TableCell>
-                <TableCell>{row.surnames}</TableCell>
-                <TableCell align="center">{row.role}</TableCell>
-                <TableCell align="center">{row.usertype}</TableCell>
-                <TableCell align="center">{row.actions}</TableCell>
+                {columns.map((column) => {
+                  const value = row[column.id];
+                  return (
+                    <TableCell key={column.id} align={column.align}>
+                      {column.format ? column.format(value) : value}
+                    </TableCell>
+                  );
+                })}
               </TableRow>
             ))}
           </TableBody>
