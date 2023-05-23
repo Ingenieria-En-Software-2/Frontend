b7e34001a18d0f7857f6260a8e7dfdf4d9b942f8
chrischriscris <18-10892@usb.ve>
2023-05-23 02:45:06 -0400

    User Roles URL changed

--- src/utils/serversUrls.ts
+++ src/utils/serversUrls.ts
@@ -5,7 +5,7 @@ export const SERVER_URLS = {
   URL_LOGIN: "/login",
   URL_USER_PROFILES: "/users",
   URL_CREATE_USER: "/users/new",
-  URL_USER_ROLES: "/users/roles",
+  URL_USER_ROLES: "/roles",
 };
 
 export default SERVER_URLS;
