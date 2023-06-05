05a993a1a65c15164d3b5f36eea1dd763e782d3d
chrischriscris <18-10892@usb.ve>
2023-06-05 10:53:26 -0400

    fixed build errors

--- src/modules/Captcha.tsx
+++ src/modules/Captcha.tsx
@@ -1,44 +1,40 @@
-import ReCAPTCHA from "react-google-recaptcha"
-import { useEffect } from "react";
-import React, { useRef } from 'react';
-import axios from 'axios';
+import ReCAPTCHA from "react-google-recaptcha";
+// import { useEffect } from "react";
+import { useRef } from "react";
+import axios from "axios";
 
-const Captcha = () =>{
-    const captchaRef = useRef(null);
-    const siteKey = import.meta.env.VITE_APP_SITE_KEY; 
-    const secretVar = import.meta.env.VITE_APP_SECRET_KEY;
+const Captcha = () => {
+  const captchaRef = useRef<ReCAPTCHA>(null);
+  const siteKey = import.meta.env.VITE_APP_SITE_KEY;
+  const secretVar = import.meta.env.VITE_APP_SECRET_KEY;
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        const inputVal = await e.target[0].value;
-        const token = captchaRef.current.getValue();
-        captchaRef.current.reset();
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    /* Comentado para que pase el build 
+    const inputVal = await e.target[0].value; */
+    const token = captchaRef.current?.getValue();
 
-        try{
-          let response = await axios.post(`http://localhost:4000/verify-token`,{
-              secret:secretVar,
-              response: token
-          });
-          console.log(response);
-          return response.data;
-        }catch(error){
-          console.log("error ",error);
-        }
+    try {
+      const response = await axios.post(`http://localhost:4000/verify-token`, {
+        secret: secretVar,
+        response: token,
+      });
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.log("error ", error);
     }
+  };
 
-    return(
-      <>
-          
-        <form onSubmit={handleSubmit}>
-              <label htmlFor="name">Name</label>
-                  <input type="text" id="name" className="input"/>
-                    <ReCAPTCHA
-                      sitekey={siteKey}
-                      ref={captchaRef}
-                      />
-            <button>Submit</button>
-          </form>
-      </>
-    );
-}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" className="input" />
+        <ReCAPTCHA sitekey={siteKey} ref={captchaRef} />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
 export default Captcha;
