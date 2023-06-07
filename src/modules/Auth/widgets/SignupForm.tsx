641ce1854ed3883668e101a6bd8506aa51a2958e
fungikami <kafung09@gmail.com>
2023-06-06 23:45:00 -0400

    added country-state-city lib

--- src/modules/Auth/widgets/SignupForm.tsx
+++ src/modules/Auth/widgets/SignupForm.tsx
@@ -1,10 +1,11 @@
 // import banner from "assets/images/banner.jpg";
+import { useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { Button, Link, MenuItem, Stack, TextField } from "@mui/material";
-import { useState } from "react";
-import _countries from "utils/countries+states+cities.json";
+import { Button, Link, MenuItem, Stack, Select, TextField, InputLabel, FormControl } from "@mui/material";
+
+import { Country, State, City } from "country-state-city";
 import Title from "components/Title";
 
 type Country = {
@@ -20,8 +21,6 @@ type Country = {
   }[];
 };
 
-const countries = _countries as Country[];
-
 const SignupForm = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -30,6 +29,35 @@ const SignupForm = () => {
   const [password, setPassword] = useState("");
   const [age, setAge] = useState("");
 
+  // -------------------- Countries, states and cities --------------------
+  // OJO PELAO, NO TODOS LOS PAISES TIENEN ESTADOS Y CIUDADES: EX. ANGUILLA
+  const countries = Country.getAllCountries();
+  const [countryCode, setCountryCode] = useState("VEN");
+
+  // Update states when country changes
+  const [states, setStates] = useState<State[]>([]);
+  const handleCountryChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    // Search iso code
+    const country = countries.find((country: Country) => country.name === e.target.value);
+    if (country) {
+      setCountryCode(country.isoCode);
+      const states = State.getStatesOfCountry(country.isoCode);
+      setStates(states);
+    }
+  };
+
+  // Update cities when state changes
+  const [cities, setCities] = useState<City[]>([]);
+  const handleStateChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    // Search state code
+    const state = states.find((state: State) => state.name === e.target.value);
+    if (state) {
+      const cities = City.getCitiesOfState(countryCode, state.isoCode);
+      setCities(cities);
+    }
+  };
+
+  // -------------------- Form submit --------------------
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(firstName, lastName, email, dateOfBirth, password);
@@ -37,7 +65,7 @@ const SignupForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} action={"" /* Login endpoint */}>
+      <FormControl onSubmit={handleSubmit} action={"" /* Login endpoint */}>
         {/* Sección 1: Información general */}
         <Title title="Información general" />
         <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
@@ -65,7 +93,7 @@ const SignupForm = () => {
             required
           />
         </Stack>
-        p
+
         {/* Correo electrónico */}
         <TextField
           type="email"
@@ -113,12 +141,11 @@ const SignupForm = () => {
         <Stack spacing={2} direction="row" sx={{ marginBottom: 4 }}>
           {/* Nacionalidad */}
           <TextField select variant="outlined" color="primary" label="Nacionalidad" fullWidth required sx={{ mb: 4 }}>
-            {/* No usar */}
-            {/* {countries.map((country: Country) => (
-              <MenuItem key={country.iso2} value={country.iso2}>
+            {countries.map((country: Country) => (
+              <MenuItem key={country.name} value={country.name}>
                 {country.name}
               </MenuItem>
-            ))} */}
+            ))}
           </TextField>
 
           {/* Documento de identidad */}
@@ -148,9 +175,18 @@ const SignupForm = () => {
         <Title title="Información de residencia" />
 
         {/* País */}
-        <TextField select variant="outlined" color="primary" label="País" fullWidth required sx={{ mb: 4 }}>
-          {countries.map((country: { code: string; name: string }) => (
-            <MenuItem key={country.code} value={country.code}>
+        <TextField
+          select
+          variant="outlined"
+          color="primary"
+          label="País"
+          fullWidth
+          required
+          sx={{ mb: 4 }}
+          onChange={handleCountryChange}
+        >
+          {countries.map((country: Country) => (
+            <MenuItem key={country.name} value={country.name}>
               {country.name}
             </MenuItem>
           ))}
@@ -158,31 +194,41 @@ const SignupForm = () => {
 
         {/* Estado o Provincia */}
         <TextField
-          type="text"
+          select
           variant="outlined"
           color="primary"
           label="Estado o Provincia"
           fullWidth
-          required
           sx={{ mb: 4 }}
-        />
+          onChange={handleStateChange}
+        >
+          {states.map((state: State) => (
+            <MenuItem key={state.name} value={state.name}>
+              {state.name}
+            </MenuItem>
+          ))}
+        </TextField>
 
         {/* Municipio o Condado */}
         <TextField
-          type="text"
+          select
           variant="outlined"
           color="primary"
           label="Municipio o Condado"
           fullWidth
-          required
           sx={{ mb: 4 }}
-        />
-
+        >
+          {cities.map((city: City) => (
+            <MenuItem key={city.name} value={city.name}>
+              {city.name}
+            </MenuItem>
+          ))}
+        </TextField>
 
         <Button variant="outlined" color="primary" type="submit" fullWidth>
           Register
         </Button>
-      </form>
+      </FormControl>
       <small>
         Already have an account?
         <Link href="/login" underline="hover">
