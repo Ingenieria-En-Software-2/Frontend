d812232e7ebc234907b1d93ce31279891a5ea29c
Carlos Sivira <carlos.csivira@gmail.com>
2023-06-10 23:42:20 -0400

    feat: signup form state management and validation

--- src/modules/Auth/widgets/SignupForm.tsx
+++ src/modules/Auth/widgets/SignupForm.tsx
@@ -1,5 +1,5 @@
 // import banner from "assets/images/banner.jpg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -39,6 +39,13 @@ function allyProps(index: number) {
 }
 
 const SignupForm = () => {
+
+  // -------------------- Form tabs --------------------
+  const [tabValue, setValue] = useState(0);
+  const handleTabChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  }
+
   // -------------------- Form states --------------------
   const [formInputs, setFormInputs] = useState<SignupFormInputs>({
     generalInfo: {
@@ -52,33 +59,114 @@ const SignupForm = () => {
       idDocument: "",
       phone: "",
     },
-    residenceInfo: { country: "", state: "", city: "", subregion: "", sector: "", street: "", room: "" },
-    workInfo: { company: "", rif: "", phone: "", country: "", state: "", city: "" },
+    residenceInfo: {
+      country: "",
+      state: "",
+      city: "",
+      subregion: "",
+      sector: "",
+      street: "",
+      room: ""
+    },
+    workInfo: {
+      company: "",
+      rif: "",
+      phone: "",
+      country: "",
+      state: "",
+      city: ""
+    },
   });
+
+  const handleFieldChange = (
+    field: keyof SignupFormInputs
+  ) => (event: any) => {
+    const { name, value } = event.target;
+    setFormInputs((prevState) => ({
+      ...prevState,
+      [field]: {
+        ...prevState[field],
+        [name]: value,
+      },
+    }));
+  };
+
+  const handleDateOfBirthChange = (date: string | null) => {
+    if (date) {
+      setFormInputs((prevState) => ({
+        ...prevState,
+        ["generalInfo"]: {
+          ...prevState["generalInfo"],
+          ["dateOfBirth"]: new Date(date).toLocaleDateString('en-US'),
+        },
+      }));
+    }
+  }
 
   // -------------------- Address inputs --------------------
   const { selected: selectedAddressHome, options: optionsHome, handlers: handlersHome } = useAddressInputs();
   const { selected: selectedAddressWork, options: optionsWork, handlers: handlersWork } = useAddressInputs();
 
-  // -------------------- Form tabs --------------------
-  const [value, setValue] = useState(0);
-  const handleChange = (_event: React.SyntheticEvent, newValue: number) => setValue(newValue);
+  
+  // -------------------- Validations --------------------
+  const [IdDocumentError, setIdDocumentError] = useState(false)
+  const validateIdDocument = (value: string | null) => {
+    const pattern = /^[VE]-\d{7,8}$/;
+    if (!value) {
+      setIdDocumentError(true);
+      return;
+    }
+    setIdDocumentError(!pattern.test(value))
+  }
+
+  const [phoneNumberError, setPhoneNumberError] = useState(false)
+  const validatePhoneNumber = (value: string | null) => {
+    const pattern = /^(?:\+)?[0-9]{0,4} ?[0-9]{10}$/;
+    if (!value) {
+      setPhoneNumberError(true);
+      return;
+    }
+    setPhoneNumberError(!pattern.test(value))
+  }
+
+  const [rifError, setRifError] = useState(false)
+  const validateRif = (value: string | null) => {
+    const pattern = /^[J]-\d{8,9}$/;
+    if (!value) {
+      setRifError(true);
+      return;
+    }
+    setRifError(!pattern.test(value))
+  }
+
+  const [companyPhoneNumberError, setCompanyPhoneNumberError] = useState(false)
+  const validateCompanyPhoneNumber = (value: string | null) => {
+    const pattern = /^(?:\+)?[0-9]{0,4} ?[0-9]{10}$/;
+    if (!value) {
+      setCompanyPhoneNumberError(true);
+      return;
+    }
+    setCompanyPhoneNumberError(!pattern.test(value))
+  }
+
+  useEffect(() => console.log(formInputs), [formInputs])
 
   return (
     <>
       {/* Tabs */}
-      <Tabs value={value} onChange={handleChange} aria-label="signup tabs">
+      <Tabs value={tabValue} onChange={handleTabChange} aria-label="signup tabs">
         <Tab label="Información general" {...allyProps(0)} />
         <Tab label="Dirección" {...allyProps(1)} />
         <Tab label="Datos de contacto" {...allyProps(2)} />
       </Tabs>
       <form onSubmit={() => null}>
-        <TabPanel value={value} index={0}>
+        <TabPanel value={tabValue} index={0}>
           {/* Tab 1: General info */}
           <Title title="Información general" />
           <Stack spacing={2} direction="row" sx={{ mb: 4 }}>
             {/* Names */}
             <TextField
+              name="names"
               type="text"
               variant="outlined"
               color="primary"
@@ -86,14 +174,28 @@ const SignupForm = () => {
               fullWidth
               required
               inputProps={{ maxLength: 20 }}
+              onChange={(event) => handleFieldChange("generalInfo")(event)}
+              value={formInputs.generalInfo.names}
             />
 
             {/* Surnames */}
-            <TextField type="text" variant="outlined" color="primary" label="Apellidos" fullWidth required />
+            <TextField 
+              name="surnames"
+              type="text" 
+              variant="outlined" 
+              color="primary" 
+              label="Apellidos" 
+              fullWidth
+              required
+              inputProps={{ maxLength: 20 }}
+              onChange={(event) => handleFieldChange("generalInfo")(event)}
+              value={formInputs.generalInfo.surnames}
+            />
           </Stack>
 
           {/* Email */}
           <TextField
+            name="email"
             type="email"
             variant="outlined"
             color="primary"
@@ -101,10 +203,13 @@ const SignupForm = () => {
             fullWidth
             required
             sx={{ mb: 4 }}
+            onChange={(event) => handleFieldChange("generalInfo")(event)}
+            value={formInputs.generalInfo.email}
           />
 
           {/* Password */}
           <TextField
+            name="password"
             type="password"
             variant="outlined"
             color="primary"
@@ -112,12 +217,15 @@ const SignupForm = () => {
             fullWidth
             required
             sx={{ mb: 4 }}
+            onChange={(event) => handleFieldChange("generalInfo")(event)}
+            value={formInputs.generalInfo.password}
           />
 
           <Stack spacing={2} direction="row" sx={{ mb: 4 }}>
             {/* Birthdate */}
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DatePicker
+                value={formInputs.generalInfo.dateOfBirth || null}
                 label="Fecha de nacimiento"
                 slotProps={{
                   textField: {
@@ -125,11 +233,13 @@ const SignupForm = () => {
                     required: true,
                   },
                 }}
+                onChange={(date) => handleDateOfBirthChange(date)}
               />
             </LocalizationProvider>
 
             {/* Gender */}
             <TextField
+              name="gender"
               select
               variant="outlined"
               color="primary"
@@ -137,6 +247,8 @@ const SignupForm = () => {
               fullWidth
               required
               sx={{ width: "60%" }}
+              onChange={(event) => handleFieldChange("generalInfo")(event)}
+              value={formInputs.generalInfo.gender}
             >
               <MenuItem value="M">Masculino</MenuItem>
               <MenuItem value="F">Femenino</MenuItem>
@@ -146,9 +258,19 @@ const SignupForm = () => {
 
           <Stack spacing={2} direction="row" sx={{ mb: 4 }}>
             {/* Nationality */}
-            <TextField select variant="outlined" color="primary" label="Nacionalidad" fullWidth required>
+            <TextField
+              name="nationality"
+              select
+              variant="outlined"
+              color="primary"
+              label="Nacionalidad"
+              fullWidth
+              required
+              onChange={(event) => handleFieldChange("generalInfo")(event)}
+              value={formInputs.generalInfo.nationality}
+            >
               {optionsHome.countries.map((country: ICountry) => (
-                <MenuItem key={country.name} value={country.name}>
+                <MenuItem key={country.name} value={country.isoCode}>
                   {country.flag} {country.name}
                 </MenuItem>
               ))}
@@ -156,20 +278,49 @@ const SignupForm = () => {
 
             {/* Identification document */}
             <TextField
+              name="idDocument"
               type="text"
               variant="outlined"
               color="primary"
               label="Documento de identificación"
               fullWidth
               required
+              error={IdDocumentError}
+              helperText={IdDocumentError 
+                ? 'El documento de identidad no es válido' : ''
+              }
+              inputProps={{ maxLength: 10}}
+              onChange={(event) => {
+                validateIdDocument(event.target.value)
+                handleFieldChange("generalInfo")(event)
+              }}
+              value={formInputs.generalInfo.idDocument}
             />
 
             {/* Phone number */}
-            <TextField type="text" variant="outlined" color="primary" label="Teléfono de contacto" fullWidth required />
+            <TextField
+              name="phone"
+              type="text"
+              variant="outlined"
+              color="primary"
+              label="Teléfono de contacto"
+              fullWidth
+              required
+              error={phoneNumberError}
+              helperText={phoneNumberError 
+                ? 'El número telefónico no es válido' : ''
+              }
+              inputProps={{ maxLength: 14}}
+              onChange={(event) => {
+                validatePhoneNumber(event.target.value)
+                handleFieldChange("generalInfo")(event)
+              }}
+              value={formInputs.generalInfo.phone}
+            />
           </Stack>
         </TabPanel>
 
-        <TabPanel value={value} index={1}>
+        <TabPanel value={tabValue} index={1}>
           {/* Tab 2: Residence info */}
           <Title title="Información de residencia" />
 
@@ -177,14 +328,15 @@ const SignupForm = () => {
             {/* Country */}
 
             <TextField
+              name="country"
               select
               variant="outlined"
               color="primary"
               label="País"
               fullWidth
               required
-              onChange={handlersHome.country}
-              value={selectedAddressHome.country}
+              onChange={(event) => handleFieldChange("residenceInfo")(event)}
+              value={formInputs.residenceInfo.country}
             >
               {optionsHome.countries.map((country: ICountry) => (
                 <MenuItem key={country.name} value={country.isoCode}>
@@ -235,14 +387,35 @@ const SignupForm = () => {
 
           <Stack spacing={2} direction="row" sx={{ mb: 4 }}>
             {/* County or municipality */}
-            <TextField type="text" variant="outlined" color="primary" label="Municipio o condado" fullWidth required />
+            <TextField
+              name="subregion"
+              type="text"
+              variant="outlined"
+              color="primary"
+              label="Municipio o condado"
+              fullWidth required
+              inputProps={{ maxLength: 20}}
+              onChange={(event) => handleFieldChange("residenceInfo")(event)}
+              value={formInputs.residenceInfo.subregion}
+            />
 
             {/* Sector */}
-            <TextField type="text" variant="outlined" color="primary" label="Sector" fullWidth required />
+            <TextField
+              name="sector"
+              type="text"
+              variant="outlined"
+              color="primary"
+              label="Sector"
+              fullWidth required
+              inputProps={{ maxLength: 20}}
+              onChange={(event) => handleFieldChange("residenceInfo")(event)}
+              value={formInputs.residenceInfo.sector}
+            />
           </Stack>
 
           {/* Street */}
           <TextField
+            name="street"
             type="text"
             variant="outlined"
             color="primary"
@@ -250,10 +423,14 @@ const SignupForm = () => {
             fullWidth
             required
             sx={{ mb: 4 }}
+            inputProps={{ maxLength: 45}}
+            onChange={(event) => handleFieldChange("residenceInfo")(event)}
+            value={formInputs.residenceInfo.street}
           />
 
           {/* Room or apartment */}
           <TextField
+            name="room"
             multiline
             rows={4}
             type="text"
@@ -263,14 +440,18 @@ const SignupForm = () => {
             fullWidth
             required
             sx={{ mb: 4 }}
+            inputProps={{ maxLength: 190}}
+            onChange={(event) => handleFieldChange("residenceInfo")(event)}
+            value={formInputs.residenceInfo.room}
           />
         </TabPanel>
-        <TabPanel value={value} index={2}>
+        <TabPanel value={tabValue} index={2}>
           {/* Tab 3: Work info */}
           <Title title="Datos de la empresa donde trabaja" />
 
           {/* Company name */}
           <TextField
+            name="company"
             type="text"
             variant="outlined"
             color="primary"
@@ -278,14 +459,34 @@ const SignupForm = () => {
             fullWidth
             required
             sx={{ mb: 4 }}
+            inputProps={{ maxLength: 45}}
+            onChange={(event) => handleFieldChange("workInfo")(event)}
+            value={formInputs.workInfo.company}
           />
 
           <Stack spacing={2} direction="row">
             {/* RIF */}
-            <TextField type="text" variant="outlined" color="primary" label="RIF" fullWidth required sx={{ mb: 4 }} />
+            <TextField
+              name="rif"
+              type="text"
+              variant="outlined"
+              color="primary"
+              label="RIF"
+              fullWidth required
+              sx={{ mb: 4 }}
+              error={rifError}
+              helperText={rifError ? 'El rif no es válido' : ''}
+              inputProps={{ maxLength: 11}}
+              onChange={(event) => {
+                validateRif(event.target.value)
+                handleFieldChange("workInfo")(event)
+              }}
+              value={formInputs.workInfo.rif}
+            />
 
             {/* Company phone number */}
             <TextField
+              name="phone"
               type="text"
               variant="outlined"
               color="primary"
@@ -293,20 +494,30 @@ const SignupForm = () => {
               fullWidth
               required
               sx={{ mb: 4 }}
+              error={companyPhoneNumberError}
+              helperText={companyPhoneNumberError 
+                ? 'El número telefónico no es válido' : ''}
+              inputProps={{ maxLength: 14}}
+              onChange={(event) => {
+                validateCompanyPhoneNumber(event.target.value)
+                handleFieldChange("workInfo")(event)
+              }}
+              value={formInputs.workInfo.phone}
             />
           </Stack>
 
           <Stack spacing={3} direction="row" sx={{ mb: 4 }}>
             {/* Country */}
             <TextField
+              name="country"
               select
               variant="outlined"
               color="primary"
               label="País"
               fullWidth
               required
-              onChange={handlersWork.country}
-              value={selectedAddressWork.country}
+              onChange={(event) => handleFieldChange("workInfo")(event)}
+              value={formInputs.workInfo.country}
             >
               {optionsWork.countries.map((country: ICountry) => (
                 <MenuItem key={country.name} value={country.isoCode}>
