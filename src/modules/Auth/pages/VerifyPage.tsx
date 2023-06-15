9db59da69fea9ac94deee63219ffb6ba6f0beb68
rcgamboan <rcgamboan@gmail.com>
2023-06-15 00:14:12 -0400

    vista usuario verificado

---
+++ src/modules/Auth/pages/VerifyPage.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useHref } from "react-router-dom";
+import SERVER_URLS from "utils/serversUrls";
+import { Button } from "@mui/material";
+
+const { URL_LOGIN } = SERVER_URLS;
+
+const VerifyPage = () => {
+  return (
+    <>  
+      <div main-container>
+        <main>
+          <h1>Su usuario ha sido verificado</h1>
+          <Button className={"text-sm text-blue-600"}>
+              <a href={URL_LOGIN} className={"text-sm text-blue-600"}>Iniciar sesión</a>
+          </Button>        
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default VerifyPage;
--- src/utils/serversUrls.ts
+++ src/utils/serversUrls.ts
@@ -7,6 +7,7 @@ export const SERVER_URLS = Object.freeze({
   URL_USER_PROFILES: "/users",
   URL_CREATE_USER: "/users/new",
   URL_USER_ROLES: "/roles",
+  URL_VERIFY: "/verify",
 });
 
 export default SERVER_URLS;
