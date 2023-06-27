5763a66812cfbd8b69deceab46caac2e904e3d5a
fungikami <kafung09@gmail.com>
2023-06-27 11:23:32 -0400

    fix errors urls

--- src/utils/serversUrls.ts
+++ src/utils/serversUrls.ts
@@ -9,7 +9,7 @@ export const SERVER_URLS = Object.freeze({
   URL_USER_ROLES: "/roles",
   URL_VERIFY: "/verify",
   URL_CREATE_ACCOUNT: "/create-account",
-  URL_NEW_TRANSACTIONS: "/new_transaction"
+  URL_NEW_TRANSACTIONS: "/new_transaction",
   URL_SAVINGS_TRANSACTION: "/savings-transaction",
   URL_CHECKING_TRANSACTION: "/checking-transaction",
 });
